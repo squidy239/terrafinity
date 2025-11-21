1eca12842f8b5d1fa8841e61e4dfdb0072321028
squidy239 <sachabarsayuracko@gmail.com>
2025-11-20 22:33:06 -0600

    cleanup

--- src/world/Structures.zig
+++
@@ -1,6 +0,0 @@
-const std = @import("std");
-const zm = @import("root").zm;
-const ztracy = @import("root").ztracy;
-
-const Block = @import("World.zig").Block;
-const WorldEditor = @import("World.zig").World.WorldEditor;
